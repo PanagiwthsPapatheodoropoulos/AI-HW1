57509cb899a664696a85303799f240a04b22433c
imhor1zon <159829126+PanagiwthsPapatheodoropoulos@users.noreply.github.com>
2025-04-10 22:25:30 +0300

    Update task3test.py

--- task3test.py
+++ task3test.py
@@ -8,8 +8,6 @@ from geopy.distance import lonlat, distance
 # Load world map
 worldmap = gpd.read_file("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
 
-# ...existing code...
-
 # Load distance data from file
 def load_distances(filename):
     """Load distances from a CSV file"""
